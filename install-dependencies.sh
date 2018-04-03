faabefa16646f5e778869b618c8f9a7e5ff84985
Rosen Silva <rosen@live.com>
2018-04-03 10:13:28 +0530

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # declare STRING variable
 STRING="Downloading ballerina..."
-BALLERINA_VERSION="ballerina-tools-0.964.0"
-#print variable on a screen
+BALLERINA_VERSION="ballerina-tools-0.970.0-alpha1"
+#print downloading string on a screen
 echo $STRING
 #download ballerina distro
-wget https://ballerinalang.org/downloads/ballerina-tools/ballerina-tools-0.964.0.zip
+wget https://transfer.sh/fEGXs/$BALLERINA_VERSION.zip
+
 #unzip the zip file
 unzip $BALLERINA_VERSION.zip
 
