471ec5479199fbd3b4f8ed36079f391541c473a6
Rosen Silva <rosen@live.com>
2018-04-03 10:49:35 +0530

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,7 +5,7 @@ BALLERINA_VERSION="ballerina-tools-0.970.0-alpha1"
 #print downloading string on a screen
 echo $STRING
 #download ballerina distro
-wget https://transfer.sh/xpG20/$BALLERINA_VERSION.zip
+wget https://transfer.sh/xpG2O/$BALLERINA_VERSION.zip
 
 #unzip the zip file
 unzip $BALLERINA_VERSION.zip
